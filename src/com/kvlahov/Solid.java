c3ba65eb32c3bd0a561793e1eb5866997f9e64e8
kvlahov <vlahov.kresimir@gmail.com>
2021-02-04 01:42:00 +0100

    Update demo

--- src/com/kvlahov/Solid.java
+++ src/com/kvlahov/Solid.java
@@ -4,7 +4,7 @@ import com.kvlahov.employees.BaseEmployee;
 import com.kvlahov.employees.BatteryFillingEmployee;
 import com.kvlahov.employees.Employee;
 import com.kvlahov.employees.GasFillingEmployee;
-import com.kvlahov.vehicles.VehicleType;
+import com.kvlahov.vehicles.*;
 
 public class Solid {
 
@@ -15,5 +15,34 @@ public class Solid {
 
         Employee ordinaryEmployee = new GasFillingEmployee(new BaseEmployee(0.1));
         Employee specialEmployee = new BatteryFillingEmployee(new BaseEmployee(0.11));
+
+        ordinaryEmployee.parkVehicle(new Car(), smallTrain);
+        ordinaryEmployee.parkVehicle(new Van(), smallTrain);
+        ordinaryEmployee.parkVehicle(new Car(), smallTrain);
+        ordinaryEmployee.parkVehicle(new Van(), smallTrain);
+
+        specialEmployee.parkVehicle(new Bus(), largeTrain);
+        specialEmployee.parkVehicle(new Bus(), largeTrain);
+        specialEmployee.parkVehicle(new Truck(), largeTrain);
+
+        System.out.println("Small train revenue: " + smallTrain.calculateRevenue());
+        System.out.println("Large train revenue: " + largeTrain.calculateRevenue());
+
+        System.out.println("Ordinary employee revenue: " + ordinaryEmployee.calculateRevenue());
+        System.out.println("Special employee revenue: " + specialEmployee.calculateRevenue());
+
+        Vehicle emptyGasCar = new Car();
+        emptyGasCar.getGas().setCurrentQuantity(0);
+
+        System.out.println("Before gas for car:" + emptyGasCar.getGas().getCurrentQuantity());
+        ordinaryEmployee.doWork(emptyGasCar);
+        System.out.println("After gas for car:" + emptyGasCar.getGas().getCurrentQuantity());
+
+        Vehicle emptyBatteryCar = new Car();
+        emptyGasCar.getBattery().setCurrentQuantity(0);
+
+        System.out.println("Before battery for car:" + emptyGasCar.getBattery().getCurrentQuantity());
+        specialEmployee.doWork(emptyGasCar);
+        System.out.println("After battery for car:" + emptyGasCar.getBattery().getCurrentQuantity());
     }
 }

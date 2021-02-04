ef847c3a40cf0bcacb3bd60705f24b3316663df3
kvlahov <vlahov.kresimir@gmail.com>
2021-02-04 01:23:21 +0100

    Start implementing demo

--- src/com/kvlahov/Solid.java
+++ src/com/kvlahov/Solid.java
@@ -1,8 +1,19 @@
 package com.kvlahov;
 
+import com.kvlahov.employees.BaseEmployee;
+import com.kvlahov.employees.BatteryFillingEmployee;
+import com.kvlahov.employees.Employee;
+import com.kvlahov.employees.GasFillingEmployee;
+import com.kvlahov.vehicles.VehicleType;
+
 public class Solid {
 
     public static void main(String[] args) {
-	// write your code here
+	    //demo
+        Train smallTrain = new Train(8, VehicleType.SMALL);
+        Train largeTrain = new Train(6, VehicleType.LARGE);
+
+        Employee ordinaryEmployee = new GasFillingEmployee(new BaseEmployee(0.1));
+        Employee specialEmployee = new BatteryFillingEmployee(new BaseEmployee(0.11));
     }
 }
